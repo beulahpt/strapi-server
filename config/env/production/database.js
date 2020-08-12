fb4ce3e717142ecce88f6676ed26e744f162f610
Beulah Sheeba <beulah@palmtreeinfotech.com>
2020-08-12 19:11:42 +0530

    Update database config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -5,8 +5,8 @@ module.exports = ({ env }) => ({
       connector: "bookshelf",
       settings: {
         client: "postgres",
-        "host": 5432,
-        "port": "${process.env.DATABASE_PORT}",
+        "host": "${process.env.DATABASE_HOST}",
+        "port": 5432,
         "database": "${process.env.DATABASE_NAME}",
         "username": "${process.env.DATABASE_USERNAME}",
         "password": "${process.env.DATABASE_PASSWORD}",
