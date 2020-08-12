701875aae47dcb881ba516334ae9e0480e8a7a74
Beulah Sheeba <beulah@palmtreeinfotech.com>
2020-08-12 19:30:22 +0530

    Update database config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: "bookshelf",
       settings: {
         client: "postgres",
-        "host": "${process.env.DATABASE_HOST}",
-        "port": 5432,
-        "database": "${process.env.DATABASE_NAME}",
-        "username": "${process.env.DATABASE_USERNAME}",
-        "password": "${process.env.DATABASE_PASSWORD}",
+        "host": "${process.env.DATABASE_HOST || '127.0.0.1'}",
+        "port": "${process.env.DATABASE_HOST || '5432'}",
+        "database": "${process.env.DATABASE_NAME || 'strapi'}",
+        "username": "${process.env.DATABASE_USERNAME || 'pt_admin'}",
+        "password": "${process.env.DATABASE_PASSWORD || 'password'}",
         schema: "public",
         ssl: { rejectUnauthorized: false }
       },
